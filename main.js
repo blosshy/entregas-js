9f4549a408c1286c006de23f290a331b10962110
Matias Martinez <matiiasss26@gmail.com>
2022-07-04 15:53:33 -0300

    se agrego comentarios en el codigo

--- main.js
+++ main.js
@@ -1,4 +1,51 @@
 
+/* FUNCION QUE ME PREGUNTE ALGO DEPENDIENDO DLE TIPO DE DATO QUE YO QUIERA */
+const preguntar = (palabra) =>{
+    let entrada = prompt("¡Estamos registrando datos para la Pokedex nacional! Por favor ingrese " + palabra);
+    return entrada;
+}
+
+/* ARRAY VACIO PARA GUARDAR MIS OBJETOS */
+const pokemones = [ ];
+
+/* CLASE CONSTRUCTURA DE OBJETOS */
+class Pokemon{
+    constructor (nombre,tipo,region,evolucion){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.region = region;
+        this.evolucion = evolucion
+    }
+}
+
+/* CICLO WHILE QUE PERMITE CREAR POKEMONES CON LA FUNCION PREGUNTAR Y PUSHEARLOS A MI ARRAY VACIO */
+let confirmacion = confirm("¿Desea registrar un Pokemon nuevo?");
+
+while (confirmacion) {
+    const pokemonNuevo = new Pokemon(preguntar("el nombre del Pokemon"),preguntar("el o los tipos del Pokemon"),preguntar("la region de procedencia del Pokemon"),preguntar("la cantidad de evoluciones que posee su Pokemon"));
+
+    pokemones.push(pokemonNuevo);
+    
+    let otroIntento = confirm("¿Desea registrar otro Pokemon?");
+    if (!otroIntento) {
+        break;
+    }
+}
+
+/* POR CADA OBJETO POKEMON EN MI ARRAY SE CREA UNA SECCION EN MI HTML */
+for (const pokemon of pokemones) {
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = `
+                <h1 class="titulo">Has registrado al Pokemon llamado: ${pokemon.nombre}</h1>
+                <p class="info">Su/s tipo/s es/son: ${pokemon.tipo} y se lo encuentra principalmente en ${pokemon.region}.</p>
+                <p class="info">Por ultimo, este Pokemon posee un total de ${pokemon.evolucion} evoluciones.
+    `;
+    document.body.appendChild(contenedor);
+}
+
+
+/* ES LA MISMA LOGICA SOLO QUE EN LUGAR DE POKEMONES USE PERSONAS */
+
 /* const preguntar = (palabra) =>{
     let entrada = prompt("Por favor ingrese su " + palabra);
     return entrada;
@@ -39,44 +86,4 @@ for (const persona of personas) {
 }
 
  */
-
-
-const preguntar = (palabra) =>{
-    let entrada = prompt("¡Estamos registrando datos para la Pokedex nacional! Por favor ingrese " + palabra);
-    return entrada;
-}
-
-const pokemones = [ ];
-
-class Pokemon{
-    constructor (nombre,tipo,region,evolucion){
-        this.nombre = nombre;
-        this.tipo = tipo;
-        this.region = region;
-        this.evolucion = evolucion
-    }
-}
-
-let confirmacion = confirm("¿Desea registrar un Pokemon nuevo?");
-
-while (confirmacion) {
-    const pokemonNuevo = new Pokemon(preguntar("el nombre del Pokemon"),preguntar("el o los tipos del Pokemon"),preguntar("la region de procedencia del Pokemon"),preguntar("la cantidad de evoluciones que posee su Pokemon"));
-
-    pokemones.push(pokemonNuevo);
-    
-    let otroIntento = confirm("¿Desea registrar otro Pokemon?");
-    if (!otroIntento) {
-        break;
-    }
-}
-
-for (const pokemon of pokemones) {
-    let contenedor = document.createElement("div");
-    contenedor.innerHTML = `
-                <h1 class="titulo">Has registrado al Pokemon llamado: ${pokemon.nombre}</h1>
-                <p class="info">Su/s tipo/s es/son: ${pokemon.tipo} y se lo encuentra principalmente en ${pokemon.region}.</p>
-                <p class="info">Por ultimo, este Pokemon posee un total de ${pokemon.evolucion} evoluciones.
-    `;
-    document.body.appendChild(contenedor);
-}
 
